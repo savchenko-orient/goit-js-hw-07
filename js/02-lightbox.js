307d1ed95990a1b4da4b40d6aafa5b98014b8314
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-02-11 10:56:41 +0200

    complete task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryLayout = galleryItems.map(({ original, description, preview }) => {
+    return ` <a class="gallery__item" href="${original}">
+                <img 
+                    class="gallery__image" 
+                     src="${preview}" 
+                     alt="${description}" 
+                />
+            </a>`;
+
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryLayout);
+
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});
